47737c25dd20b974f3711806e2b2adefee0c5bfb
olerom <mefiader@gmail.com>
2017-05-04 12:39:26 +0300

    Add pseudocode for ore_dirac alorithm, start writing coursework

--- hamiltonian_cycle/algorithms/ore_dirac.py
+++ hamiltonian_cycle/algorithms/ore_dirac.py
@@ -8,7 +8,8 @@ def ore_dirac(graph):
     for k in range(n * (n - 1)):
         if graph[path[0]][path[1]] != 1:
             j = 2
-            while j < n and ((graph[path[0]][path[j]] != 1) or (graph[path[1]][path[j + 1]] != 1)):
+
+            while (j < n) and ((graph[path[0]][path[j]] != 1) or (graph[path[1]][path[j + 1]] != 1)):
                 j += 1
             path = reverse_sublist(path, 1, j + 1)
         path.append(path.pop(0))
