6310dc519224377e3d43ff2d06533cd68c97e7a0
olerom <mefiader@gmail.com>
2017-05-09 03:23:54 +0300

    OreDirac class can check Ore and Dirac conditions now

--- src/main/ham_cycle.py
+++ src/main/ham_cycle.py
@@ -6,6 +6,9 @@ recursive = Recursive(hamiltonian_matrix)
 
 ore_dirac = OreDirac(ore_dirac_graph)
 
+print(ore_dirac.is_dirac())
+print(ore_dirac.is_ore())
+
 print("Recursive algorithm:", recursive.get_cycle())
 
 print("Algorithm for graph that satisfy ore or dirac theorems:", ore_dirac.get_cycle())
--- src/main/ore_dirac.py
+++ src/main/ore_dirac.py
@@ -22,6 +22,50 @@ class OreDirac:
             path.append(path.pop(0))
         return path
 
+    def is_dirac(self):
+        n = len(self.graph)
+        if n < 3:
+            return False
+
+        for i in range(n):
+            counter = 0
+
+            for j in range(n):
+                if self.graph.is_connected(i, j):
+                    counter += 1
+
+            if counter < n / 2:
+                return False
+
+        return True
+
+    def is_ore(self):
+        n = len(self.graph)
+        if n < 3:
+            return False
+
+        edges = []
+        for i in range(n):
+            for j in range(n):
+                edges.append([i, j])
+
+        vertex_deg = {}
+
+        for i in range(n):
+            counter = 0
+            for j in range(n):
+                if self.graph.is_connected(i, j):
+                    edges.remove([i, j])
+                    counter += 1
+
+            vertex_deg.update({i: counter})
+
+        for edge in edges:
+            if vertex_deg.get(edge[0]) + vertex_deg.get(edge[1]) < n:
+                return False
+
+        return True
+
     @staticmethod
     def reverse_sublist(lst, start, end):
         lst[start:end] = lst[start:end][::-1]
